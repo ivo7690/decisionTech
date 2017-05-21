934e31c8139b755a0a2d4bfff5c980f1c12e4a68
Ivan Li Trenta <ivanlitrenta@Ivans-MacBook-Pro.local>
2017-05-21 22:54:42 +0100

    change

--- technicaltest-seedproject/decision-tech-angularjs-seed/src/main.js
+++ technicaltest-seedproject/decision-tech-angularjs-seed/src/main.js
@@ -4,11 +4,3 @@ angular.module('app', []);
 
 require('./components/home/home.component');
 import './styles.scss';
-
-$(function() {
-
-    $('#menu').click(function() {
-
-        $('#filterContainer').toggleClass('filterShow');
-    })
-})
