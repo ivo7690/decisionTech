33a3770670eccf72972bfa3a853045efa7e0e453
Ivan Li Trenta <ivanlitrenta@Ivans-MacBook-Pro.local>
2017-05-22 16:47:33 +0100

    add npm jquery

---
+++ technicaltest-seedproject/decision-tech-angularjs-seed/src/Components/home/home.component.spec.js
@@ -0,0 +1,31 @@
+describe('Start test unit', () => {
+    var $componentController;
+    beforeEach(angular.mock.module('app'));
+    beforeEach(angular.mock.module(function($provide) {
+        $provide.value('home.service', {
+            getDeals: () => {
+                return { then: (callback) => callback([{ name: 'x' }]) }
+            }
+        })
+    }));
+
+
+    // beforeEach(angular.mock.inject(function(_$componentController_) {
+    //     $componentController = _$componentController_;
+    // }));
+
+    // it('should expose a `hero` object', function() {
+
+    //     const ctrl = $componentController('home', null, {});
+
+    //     expect(ctrl.deals).toBeDefined();
+    //     expect(ctrl.deals[0].title).toBe('Unlimited Broadband & Weekend Calls');
+    // });
+    describe('Test 1', () => {
+        it('Test case 1', function() {
+            var x = true;
+            expect(x).toBe(true);
+        });
+    });
+
+});
--- technicaltest-seedproject/decision-tech-angularjs-seed/src/main.js
+++ technicaltest-seedproject/decision-tech-angularjs-seed/src/main.js
@@ -1,6 +1,13 @@
 import angular from 'angular';
+var $ = require("jquery");
 
 angular.module('app', []);
 
 require('./components/home/home.component');
 import './styles.scss';
+
+$(function() {
+    $('#menu').click(function() {
+        $('#filterContainer').toggleClass('filterShow');
+    })
+})
